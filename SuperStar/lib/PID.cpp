36f0e1edbf9710bb49445230c01e7dc23f370787
Abdelbadi Lababidi <20369@ecam.be>
2025-03-14 13:16:13 +0100

    Ajout du contrôleur PID (PID.h/.cpp)

---
+++ SuperStar/lib/PID.cpp
@@ -0,0 +1,10 @@
+#include "PID.h"
+
+PID::PID(float kp, float ki, float kd) : kp(kp), ki(ki), kd(kd), previousError(0), integral(0) {}
+
+float PID::compute(float error) {
+    integral += error;
+    float derivative = error - previousError;
+    previousError = error;
+    return kp * error + ki * integral + kd * derivative;
+}
